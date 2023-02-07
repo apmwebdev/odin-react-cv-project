24a7881d558ced9e20a600bf3d88051de2a094c2
Austin Miller <austinmillerwebdev@gmail.com>
2023-02-07 09:54:02 -0800

    Change education form labels to use htmlFor

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -17,49 +17,42 @@ class EducationForm extends Component {
         onSubmit={this.props.formSubmitHandler}
       >
         <div className="experienceFormRow">
-          <label>
-            School name:&nbsp;
-            <input
-              type="text"
-              name="schoolName"
-              placeholder="School name"
-              onInput={this.props.inputChangeHandler}
-            />
-          </label>
+          <label htmlFor='schoolName'>School name:</label>
+          <input
+            type="text"
+            name="schoolName"
+            placeholder="School name"
+            onInput={this.props.inputChangeHandler}
+          />
         </div>
         <div className="experienceFormRow">
-          <label>
-            Degree earned:&nbsp;
-            <input
-              type="text"
-              name="degree"
-              placeholder="Degree earned"
-              onInput={this.props.inputChangeHandler}
-            />
-          </label>
-          <label>
-            Major/Concentration: &nbsp;
-            <input
-              type="text"
-              name="major"
-              placeholder="Major"
-              onInput={this.props.inputChangeHandler}
-            />
-          </label>
-          <label>
-            GPA: &nbsp;
-            <input type="text" name="gpa" placeholder="GPA" onInput={this.props.inputChangeHandler} />
-          </label>
+          <label htmlFor='degree'>Degree earned:</label>
+          <input
+            type="text"
+            name="degree"
+            placeholder="Degree earned"
+            onInput={this.props.inputChangeHandler}
+          />
+          <label htmlFor='major'>Major/Concentration:</label>
+          <input
+            type="text"
+            name="major"
+            placeholder="Major"
+            onInput={this.props.inputChangeHandler}
+          />
+          <label htmlFor='gpa'>GPA:</label>
+          <input
+            type="text"
+            name="gpa"
+            placeholder="GPA"
+            onInput={this.props.inputChangeHandler}
+          />
         </div>
         <div className="experienceFormRow">
-          <label>
-            From: &nbsp;
-            <input type="month" name="from_date" onInput={this.props.inputChangeHandler} />
-          </label>
-          <label>
-            To: &nbsp;
-            <input type="month" name="to_date" onInput={this.props.inputChangeHandler} />
-          </label>
+          <label htmlFor='from_date'>From:</label>
+          <input type="month" name="from_date" onInput={this.props.inputChangeHandler} />
+          <label htmlFor='to_date'>To:</label>
+          <input type="month" name="to_date" onInput={this.props.inputChangeHandler} />
         </div>
         <div className="experienceFormRow">
           <button type="submit">Submit</button>
